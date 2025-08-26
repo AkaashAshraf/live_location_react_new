a72e97d80abf9f51a4bbd2ae3f3750c5cc57c7b7
Akash <akash@192.168.1.14>
2025-08-26 08:36:23 +0500

    done

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
-export const BASE_URL = "http://16.16.96.70:3000/api";
-export const SOCKET_URL = "http://16.16.96.70:3000";
+export const BASE_URL = "http://13.50.224.4:3000/api";
+export const SOCKET_URL = "http://13.50.224.4:3000";
 export const GOOGLE_MAPS_API_KEY = "AIzaSyDcu3TPs4TtGv4gt7us_Zax9p9DW8JfqyA"; // put your key here
 export const DEFAULT_DRIVER_IMAGE = "https://pixabay.com/vectors/avatar-people-person-business-user-3637561"; // static driver image
